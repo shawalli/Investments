2e9b16c8609e1f6ab5345835ed03b78f6607c88c
Shawn Wallis <shawn.p.wallis@gmail.com>
2016-11-19 14:18:58 -0500

    windowed

--- Investments.py
+++ Investments.py
@@ -1,6 +1,15 @@
 import Tkinter as tk
 
-root = tk.Tk()
-root.title("Investments")
-tk.Button(root, text="Here are your investments.").pack()
-tk.mainloop()
+NAME = "Investments"
+WIDTH = 400
+HEIGHT = 400
+
+class App(object):
+    def __init__(self):
+        self.app = tk.Tk()
+        self.app.title(NAME)
+        self.app.geometry('%dx%d' % (WIDTH, HEIGHT))
+        self.app.mainloop()
+
+if __name__ == '__main__':
+    App()
