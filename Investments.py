2134570f072b53abd978cc6d3ec86c3ab6361435
Shawn Wallis <shawn.p.wallis@gmail.com>
2016-11-19 20:00:01 -0500

    dialog to open file

--- Investments.py
+++ Investments.py
@@ -1,12 +1,17 @@
+
+from functools import partial
+
 import Tkinter as tk
+from tkFileDialog import askopenfilename
 
 NAME = "Investments"
 VERSION="0.1.0"
 WIDTH = 400
 HEIGHT = 400
 
-def load_csv():
-    pass
+def load_csv(app):
+    csv_filename = askopenfilename(parent=app)
+    # todo: pop up small spreadsheet window to choose spreadsheet ranges
 
 def save_session_to_file():
     pass
@@ -43,7 +48,7 @@ class App(object):
         menu.add_cascade(menu=appmenu)
 
         filemenu = tk.Menu(menu, tearoff=0)
-        filemenu.add_command(label="Add CSV", command=load_csv)
+        filemenu.add_command(label="Add CSV", command=partial(load_csv, self.app))
         filemenu.add_command(label="Save Session", command=save_session_to_file)
         filemenu.add_command(label="Quit", command=self.app.quit)
         menu.add_cascade(label="File", menu=filemenu)
